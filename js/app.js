50e37ff4c8141781dfbf9c5d8630fd556cc3db4f
Maxime <mcluzel@edenschool.fr>
2022-03-11 16:07:49 +0100

    Il n'y a que 24:00 qui s'affiche mais le JS ne fonctionne pas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,51 @@
-// DOM - GET SPAN BY THEIR ID
-const first_nb = document.getElementsByClassName('first_nb');
-const second_nb = document.getElementsByClassName('second_nb');
-const third_nb = document.getElementsByClassName('third_nb');
-const fourth_nb = document.getElementsByClassName('fourth_nb');
+// DOM - Get Article By Their ID
+const first_nb = document.getElementById('first_nb');
+const second_nb = document.getElementById('second_nb');
+const third_nb = document.getElementById('third_nb');
+const fourth_nb = document.getElementById('fourth_nb');
 
-// DOM - MAKE LINK BETWEEN SPAN ID and Span variable
-let hour1 = 
+// DOM - Get Span By Their Class
+// First Article
+let f_bar1 = document.getElementsByClassName('f_bar1');
+let f_bar2 = document.getElementsByClassName('f_bar2');
+let f_bar3 = document.getElementsByClassName('f_bar3');
+let f_bar4 = document.getElementsByClassName('f_bar4');
+let f_bar5 = document.getElementsByClassName('f_bar5');
+let f_bar6 = document.getElementsByClassName('f_bar6');
+let f_bar7 = document.getElementsByClassName('f_bar7');
+
+// Second Article
+let s_bar1 = document.getElementsByClassName('s_bar1');
+let s_bar2 = document.getElementsByClassName('s_bar2');
+let s_bar3 = document.getElementsByClassName('s_bar3');
+let s_bar4 = document.getElementsByClassName('s_bar4');
+let s_bar5 = document.getElementsByClassName('s_bar5');
+let s_bar6 = document.getElementsByClassName('s_bar6');
+let s_bar7 = document.getElementsByClassName('s_bar7');
+
+// Third Article
+let t_bar1 = document.getElementsByClassName('t_bar1');
+let t_bar2 = document.getElementsByClassName('t_bar2');
+let t_bar3 = document.getElementsByClassName('t_bar3');
+let t_bar4 = document.getElementsByClassName('t_bar4');
+let t_bar5 = document.getElementsByClassName('t_bar5');
+let t_bar6 = document.getElementsByClassName('t_bar6');
+let t_bar7 = document.getElementsByClassName('t_bar7');
+
+// Fourth Article
+let q_bar1 = document.getElementsByClassName('q_bar1');
+let q_bar2 = document.getElementsByClassName('q_bar2');
+let q_bar3 = document.getElementsByClassName('q_bar3');
+let q_bar4 = document.getElementsByClassName('q_bar4');
+let q_bar5 = document.getElementsByClassName('q_bar5');
+let q_bar6 = document.getElementsByClassName('q_bar6');
+let q_bar7 = document.getElementsByClassName('q_bar7');
+
+let hours = first_nb;
+let hours2 = second_nb;
+let min = third_nb;
+let min2 = fourth_nb;
+
+function timer(){
+  
+}
