99c6a8e72fa4018e0b6490fa94da0c723a02bba5
Maxime <mcluzel@edenschool.fr>
2022-03-11 14:00:55 +0100

    Introduce project ( HMTL CSS JS files)

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// DOM - GET SPAN BY THEIR ID
+const first_nb = document.getElementsByClassName('first_nb');
+const second_nb = document.getElementsByClassName('second_nb');
+const third_nb = document.getElementsByClassName('third_nb');
+const fourth_nb = document.getElementsByClassName('fourth_nb');
+
+// DOM - MAKE LINK BETWEEN SPAN ID and Span variable
+let hour1 = 
