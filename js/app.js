cec377f8a0ae9f7da66ab0b8f5166523c420e6e5
Max-691 <mcluzel@edenschool.fr>
2022-04-04 21:41:11 +0200

    I think I've understand functionement. I'm forwarding... Add of gitignore

--- js/app.js
+++ js/app.js
@@ -26,12 +26,51 @@ const timer = setInterval(function(){
     } else {
       time += secondes;
     }
+
+    // IF 0
     for (let index = 0; index < time.length; index++) {
       if(time[index] === "0"){
+        // TOP DIV
         lis[index].firstElementChild.classList.remove("bas");
-        lis[index].firstElementChild.classList.add("gauche haut droite");
+        lis[index].firstElementChild.classList.add("gauche");
+        lis[index].firstElementChild.classList.add("haut");
+        lis[index].firstElementChild.classList.add("droite");
+        // BOTTOM DIV
+        lis[index].lastElementChild.classList.remove("haut");
+        lis[index].firstElementChild.classList.add("gauche");
+        lis[index].firstElementChild.classList.add("bas"); 
+        lis[index].firstElementChild.classList.add("droite");
+      }
+
+      // IF 1
+      if(time[index] === "1"){
+        // TOP DIV
+        lis[index].firstElementChild.classList.add("gauche");
+        lis[index].firstElementChild.classList.remove("droite");
+        lis[index].firstElementChild.classList.remove("bas");
         lis[index].firstElementChild.classList.remove("haut");
-        lis[index].firstElementChild.classList.add("gauche bas droite");
+
+        // BOTTOM DIV
+        lis[index].lastElementChild.classList.add("gauche");
+        lis[index].lastElementChild.classList.remove("droite");
+        lis[index].lastElementChild.classList.remove("bas");
+        lis[index].lastElementChild.classList.remove("haut");
+      }
+
+      // IF 2
+      if(time[index] === "2"){
+        lis[index].firstElementChild.classList.add("haut");
+        lis[index].firstElementChild.classList.add("droite");
+        lis[index].firstElementChild.classList.add("bas");
+        lis[index].firstElementChild.classList.add("gauche");
+        lis[index].firstElementChild.classList.add("bas");
+      }
+
+
+
+      else{
+        lis[index].firstElementChild.classList.add("orange");
+        lis[index].lastElementChild.classList.add("blue");
       }
       
     }
