5cf7d5ee00e9ccacebd614b03b98aa6a5dc53375
Maxime <mcluzel@edenschool.fr>
2022-05-23 16:35:24 +0200

    Add operationnal timer function

--- correction/clock_correction/main.js
+++ correction/clock_correction/main.js
@@ -1,1 +1,37 @@
 import './style.css'
+
+let minutes = 24;
+let seconds = 0;
+
+const timer = setInterval(()=>{
+  let tmp = "";
+  if (minutes < 10) {
+    tmp += "0" + minutes
+  }
+  else{
+    tmp += minutes;
+  }
+
+  if (seconds < 10) {
+    tmp += "0" + seconds
+  }
+  else{
+    tmp += seconds;
+  }
+
+  console.log(tmp);
+
+
+  if (seconds === 0) {
+    if (minutes === 0) {
+      clearInterval(timer)
+    }
+    else{
+      minutes--
+      seconds = 59
+    }
+  }
+
+  else{ seconds--}
+  
+}, 100)
