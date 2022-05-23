79754d9baa2d5b3c1d66c1eae64fe0aff5b65e84
Maxime <mcluzel@edenschool.fr>
2022-05-23 17:14:50 +0200

    Add half correction

--- correction/clock_correction/main.js
+++ correction/clock_correction/main.js
@@ -3,6 +3,9 @@ import './style.css'
 let minutes = 24;
 let seconds = 0;
 
+const huits = document.querySelector('.huit')
+console.log(huits)
+
 const timer = setInterval(()=>{
   let tmp = "";
   if (minutes < 10) {
@@ -19,7 +22,20 @@ const timer = setInterval(()=>{
     tmp += seconds;
   }
 
-  console.log(tmp);
+  for(let i = 0; i < huits.length; i++){
+    const huit = huits[i]
+    const number = tmp[i]
+    for (const tile of huit.children) {
+      tile.classList.remove('on')
+      tile.classList.add('off')
+      if (number === "0") {
+        if (tile.classList.contains('top')) {
+          tile.classList.remove('off')
+          tile.classList.add('on')
+        }
+      }
+    }
+  }
 
 
   if (seconds === 0) {
